4a425f1a156cfe583ba259ed46aeb7cf1c219543
Zach Thomas <zach.thomas1.zt@gmail.com>
2023-01-10 14:41:26 -0600

    Add org bullets

--- init.el
+++ init.el
@@ -269,21 +269,26 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
+
 (use-package org-roam
-:after org
-:init (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
-:custom
-(org-roam-directory (file-truename org-directory))
-:config
-(org-roam-setup)
-:bind (("C-c n f" . org-roam-node-find)
-	("C-c n r" . org-roam-node-random)		    
-	(:map org-mode-map
-		(("C-c n i" . org-roam-node-insert)
+  :after org
+  :init (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :config
+  (org-roam-setup)
+  :bind (("C-c n f" . org-roam-node-find)
+	 ("C-c n r" . org-roam-node-random)		    
+	 (:map org-mode-map
+	       (("C-c n i" . org-roam-node-insert)
 		("C-c n o" . org-id-get-create)
 		("C-c n t" . org-roam-tag-add)
 		("C-c n a" . org-roam-alias-add)
 		("C-c n l" . org-roam-buffer-toggle)))))
+
+(use-package org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
 (setq browse-url-browser-function 'eww-browse-url
     shr-use-colors nil
     shr-bullet "• "
