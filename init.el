9f18b500449668670a86bfb51578167063b1f3fb
Zach Thomas <zach.thomas1.zt@gmail.com>
2023-03-17 13:30:11 -0500

    Whitespace mode stuff

--- init.el
+++ init.el
@@ -578,3 +578,30 @@
 (setq cperl-invalid-face nil)
 
 (modify-syntax-entry ?_ "w")
+
+;; Define the whitespace style.
+(setq-default whitespace-style
+              '(face spaces empty tabs newline trailing space-mark tab-mark newline-mark))
+;; Whitespace color corrections.
+(require 'color)
+(let* ((ws-lighten 30) ;; Amount in percentage to lighten up black.
+       (ws-color (color-lighten-name "#000000" ws-lighten)))
+  (custom-set-faces
+   `(whitespace-newline                ((t (:foreground ,ws-color))))
+   `(whitespace-missing-newline-at-eof ((t (:foreground ,ws-color))))
+   `(whitespace-space                  ((t (:foreground ,ws-color))))
+   `(whitespace-space-after-tab        ((t (:foreground ,ws-color))))
+   `(whitespace-space-before-tab       ((t (:foreground ,ws-color))))
+   `(whitespace-tab                    ((t (:foreground ,ws-color))))
+   `(whitespace-trailing               ((t (:foreground ,ws-color))))))
+;; Make these characters represent whitespace.
+(setq-default whitespace-display-mappings
+      '(
+        ;; space -> · else .
+        (space-mark 32 [183] [46])
+        ;; new line -> ¬ else $
+        (newline-mark ?\n [172 ?\n] [36 ?\n])
+        ;; carriage return (Windows) -> ¶ else #
+        (newline-mark ?\r [182] [35])
+        ;; tabs -> » else >
+        (tab-mark ?\t [187 ?\t] [62 ?\t])))
