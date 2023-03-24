51a47ea48a685846b414736aa09f82bdec001c05
Zach Thomas <zach.thomas1.zt@gmail.com>
2023-03-24 13:51:36 -0500

    Add yassnippet

--- init.el
+++ init.el
@@ -615,3 +615,14 @@
   :ensure t
   :init
   (global-undo-tree-mode))
+
+(use-package yasnippet
+  :ensure t
+  :hook ((text-mode
+          prog-mode
+          conf-mode
+          snippet-mode) . yas-minor-mode-on)
+  :init
+  (setq yas-snippet-dir "~/.emacs.d/snippets"))
+
+(use-package yasnippet-snippets)
