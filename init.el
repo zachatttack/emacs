d8ba5d06d75217c03ed864bbd6cdc70ed3a7a4ce
Zach Thomas <zach.thomas1.zt@gmail.com>
2023-07-18 10:07:04 -0500

    Add evil back in

--- init.el
+++ init.el
@@ -43,10 +43,67 @@
 
 (use-package no-littering)
 
+
+(defun zt/evil-hook ()
+  (dolist (mode '(custom-mode
+                  eshell-mode
+                  git-rebase-mode
+                  erc-mode
+                  circe-server-mode
+                  circe-chat-mode
+                  circe-query-mode
+                  sauron-mode
+                  term-mode))
+   (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump t)
+  (setq evil-undo-system 'undo-fu)
+  (setq evil-want-minibuffer nil)
+  :hook (evil-mode . zt/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal)
+  ;;emacs-mode instead of insert mode
+  (defalias 'evil-insert-state 'evil-emacs-state)
+  (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
+  (setq evil-emacs-state-cursor '(bar . 1))
+  )
+
+(use-package evil-goggles
+  :ensure t
+  :init 
+  (setq evil-goggles-duration 0.100)
+  :config
+  (evil-goggles-mode))
+
+(use-package evil-collection
+  :after evil
+  :custom (evil-collection-setup-minibuffer nil)
+  :config
+  (evil-collection-init))
+
+(use-package evil-nerd-commenter
+  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
 (use-package kaolin-themes
  :config
   (load-theme 'kaolin-temple t)
  (kaolin-treemacs-theme))
+ 
+ 
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
@@ -60,6 +117,9 @@
 		pdf-view-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+
 
 
 (use-package rainbow-delimiters
