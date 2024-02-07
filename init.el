1617dae014800b7688d52c25d77cfca9c5e88f7e
Zach Thomas <zach.thomas1.zt@gmail.com>
2024-02-07 10:54:54 -0600

    Add time date stuff

--- init.el
+++ init.el
@@ -980,3 +980,28 @@ isn't there and triggers an error"
   (openwith-mode t)
   (setq openwith-associations '(("\\.pdf\\'" "C:/Program Files/Google/Chrome/Application/chrome.exe" (file))))
   )
+
+(defvar current-date-format "%Y-%m-%d"
+  "Format of date to insert with `insert-current-date-time' func
+See help of `format-time-string' for possible replacements")
+
+(defvar current-time-format "%a %H:%M:%S"
+  "Format of date to insert with `insert-current-time' func.
+Note the weekly scope of the command's precision.")
+
+(defun insert-current-date ()
+  "insert the current date and time into current buffer.
+Uses `current-date-time-format' for the formatting the date/time."
+       (interactive)
+       (insert (format-time-string current-date-format (current-time)))
+       )
+
+(defun insert-current-time ()
+  "insert the current time (1-week scope) into the current buffer."
+       (interactive)
+       (insert (format-time-string current-time-format (current-time)))
+       (insert "\n")
+       )
+
+(global-set-key "\C-c\C-d" 'insert-current-date)
+(global-et-key "\C-c\C-t" 'insert-current-time)
